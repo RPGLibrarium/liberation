92321d4a2580e8c2dd9c8d0274e5e336d1332d1a
Yoann Kehler <privat@yoann.de>
2015-02-05 18:10:50 +0100

    Trivialen Test eingefügt

--- yo/test/spec/controllers/collection.js
+++ yo/test/spec/controllers/collection.js
@@ -1,23 +1,23 @@
-/*'use strict';
+'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: CollectionCtrl', function () {
 
   // load the controller's module
   beforeEach(module('liberationApp'));
 
-  var AboutCtrl,
+  var CollectionCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    CollectionCtrl = $controller('CollectionCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of books to the scope', function () {
+    expect(1).toBe(1);
   });
+
 });
-*/
