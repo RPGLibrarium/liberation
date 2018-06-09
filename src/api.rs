b64f9111064504ef57b2e5be49fada1c0bc5faa6
Yoann Kehler <privat@yoann.de>
2018-06-09 10:20:09 +0200

    Added function signatures for rpgsystems, books, guilds

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,4 @@
-use actix_web::{server, App, HttpRequest};
+use actix_web::{server, App, HttpRequest, Responder};
 use database::Database;
 
 #[derive(Clone)]
@@ -12,7 +12,58 @@ pub fn get_v1(state: AppState) -> Box<server::HttpHandler> {
     .boxed()
 }
 
+fn get_rpg_systems(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET RpgSystem"
+}
 
-fn index(_req: HttpRequest) -> &'static str {
-    "Hello world!"
+fn get_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET RpgSystem by Id"
+}
+
+fn post_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
+    "POST RpgSystem"
+}
+
+fn put_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
+    "PUT RpgSystem"
+}
+
+fn get_books(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET Books"
+}
+
+fn get_book(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET Book by Id"
+}
+
+fn post_book(_req: HttpRequest<AppState>) -> impl Responder {
+    "POST Book"
+}
+
+fn put_Book(_req: HttpRequest<AppState>) -> impl Responder {
+    "PUT Book"
+}
+
+fn get_guilds(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET Guilds"
+}
+
+fn get_guild(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET Guild by Id"
+}
+
+fn get_guild_inventory(_req: HttpRequest<AppState>) -> impl Responder {
+    "GET Guild inventory by Id"
+}
+
+fn post_guild(_req: HttpRequest<AppState>) -> impl Responder {
+    "POST Guild"
+}
+
+fn put_guild(_req: HttpRequest<AppState>) -> impl Responder {
+    "PUT Guild"
+}
+
+fn post_guild_inventory(_req: HttpRequest<AppState>) -> impl Responder {
+    "POST Guild Inventory"
 }
