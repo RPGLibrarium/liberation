ac98093365455a292b286f584fdb9dc159d53566
Hanna Franzen <hanna.franzen@rwth-aachen.de>
2018-11-10 16:06:03 +0100

    added basic doc comments to api mod

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -15,12 +15,17 @@ use database::*;
 use error::Error;
 use futures::future::{err, result, Future};
 use std::sync::Arc;
+
+/// Handling of external modules
 #[derive(Clone)]
 pub struct AppState {
+    /// Underlaying database
     pub db: Database,
+    /// Keycloak for authentification
     pub kc: KeycloakCache,
 }
 
+/// Getter for web folder
 pub fn get_static() -> Box<dyn server::HttpHandler<Task = Box<HttpHandlerTask>>> {
     App::new()
         .prefix("/web")
@@ -82,6 +87,7 @@ pub fn get_v1(state: AppState) -> Box<dyn server::HttpHandler<Task = Box<HttpHan
 // - Json<T>
 // https://actix.rs/actix-web/actix_web/trait.Responder.html
 
+/// Get all RpgSystems (if authentification is successful)
 fn get_rpg_systems(_req: HttpRequest<AppState>) -> impl Responder {
     assert_roles(&_req, vec![])?;
 
@@ -91,6 +97,7 @@ fn get_rpg_systems(_req: HttpRequest<AppState>) -> impl Responder {
     // Response<Json<T>, Into<Error>> = impl Response
 }
 
+/// Get a requested RpgSystem (if authentification is successful)
 fn get_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
     assert_roles(&_req, vec![])?;
 
@@ -99,6 +106,7 @@ fn get_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
     bus::get_rpgsystem(&_req.state().db, id).and_then(|system| Ok(Json(system)))
 }
 
+/// Insert a new RpgSystem (if authentification is successful)
 fn post_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
     let claims = assert_roles(&_req, vec![ROLE_ADMIN, ROLE_LIBRARIAN])?;
 
@@ -115,6 +123,7 @@ fn post_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
         }).responder())
 }
 
+/// Update an existing RpgSystem (if authentification is successful)
 fn put_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
     let claims = assert_roles(&_req, vec![ROLE_ADMIN, ROLE_LIBRARIAN])?;
 
@@ -132,6 +141,7 @@ fn put_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
         .responder())
 }
 
+/// Delete a given RpgSystem (if authentification is successful)
 fn delete_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
     let claims = assert_roles(&_req, vec![ROLE_ADMIN, ROLE_LIBRARIAN])?;
 
@@ -142,10 +152,12 @@ fn delete_rpg_system(_req: HttpRequest<AppState>) -> impl Responder {
         .and_then(|_| Ok(HttpResponse::NoContent().finish()))
 }
 
+/// Get all Titles (if authentification is successful)
 fn get_titles(_req: HttpRequest<AppState>) -> impl Responder {
     bus::get_titles(&_req.state().db).and_then(|titles| Ok(Json(titles)))
 }
 
+/// Get a requested Title (if authentification is successful)
 fn get_title(_req: HttpRequest<AppState>) -> impl Responder {
     let id: TitleId = _req.match_info().query("titleid")?;
     let claims = get_claims_for_req(&_req)?;
@@ -154,6 +166,7 @@ fn get_title(_req: HttpRequest<AppState>) -> impl Responder {
         .map_err(Error::from)
 }
 
+/// Insert a new Title (if authentification is successful)
 fn post_title(_req: HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Error = Error>> {
     let claims = match get_claims_for_req(&_req) {
         Err(_) => return Box::new(err(Error::from(error::Error::InvalidAuthenticationError))),
@@ -173,6 +186,7 @@ fn post_title(_req: HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Er
         .responder()
 }
 
+/// Update an existing Title (if authentification is successful)
 fn put_title(_req: HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Error = Error>> {
     let claims = match get_claims_for_req(&_req) {
         Err(_) => return Box::new(err(Error::from(error::Error::InvalidAuthenticationError))),
@@ -196,6 +210,7 @@ fn put_title(_req: HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Err
         .responder()
 }
 
+/// Get all Books (if authentification is successful)
 fn get_books(_req: HttpRequest<AppState>) -> impl Responder {
     let claims = get_claims_for_req(&_req)?;
     // TODO roles
@@ -203,54 +218,67 @@ fn get_books(_req: HttpRequest<AppState>) -> impl Responder {
     bus::get_books(&_req.state().db, claims).and_then(|books| Ok(Json(books)))
 }
 
+/// Get a requested Book (if authentification is successful)
 fn get_book(_req: HttpRequest<AppState>) -> impl Responder {
     "GET Book by Id"
 }
 
+/// Insert a new Book (if authentification is successful)
 fn post_book(_req: HttpRequest<AppState>) -> impl Responder {
     "POST Book"
 }
 
+/// Update an existing Book (if authentification is successful)
 fn put_book(_req: HttpRequest<AppState>) -> impl Responder {
     "PUT Book"
 }
 
+/// Get all Members (if authentification is successful)
 fn get_members(_req: HttpRequest<AppState>) -> impl Responder {
     "GET members"
 }
 
+/// Get a requested Member (if authentification is successful)
 fn get_member(_req: HttpRequest<AppState>) -> impl Responder {
     "GET members/<id>"
 }
 
+/// Get the inventory of a Member (if authentification is successful)
 fn get_member_inventory(_req: HttpRequest<AppState>) -> impl Responder {
     "GET members/<id>/inventory"
 }
 
+/// Insert into a member's inventory (if authentification is successful)
 fn post_member_inventory(_req: HttpRequest<AppState>) -> impl Responder {
     "POST members/<id>/inventory"
 }
 
+/// Get all Guilds (if authentification is successful)
 fn get_guilds(_req: HttpRequest<AppState>) -> impl Responder {
     "GET Guilds"
 }
 
+/// Get a requested Guild (if authentification is successful)
 fn get_guild(_req: HttpRequest<AppState>) -> impl Responder {
     "GET Guild by Id"
 }
 
+/// Get the inventory of a Guild (if authentification is successful)
 fn get_guild_inventory(_req: HttpRequest<AppState>) -> impl Responder {
     "GET Guild inventory by Id"
 }
 
+/// Insert a new Guild (if authentification is successful)
 fn post_guild(_req: HttpRequest<AppState>) -> impl Responder {
     "POST Guild"
 }
 
+/// Update an existing Guild (if authentification is successful)
 fn put_guild(_req: HttpRequest<AppState>) -> impl Responder {
     "PUT Guild"
 }
 
+/// Insert into a guild's inventory (if authentification is successful)
 fn post_guild_inventory(_req: HttpRequest<AppState>) -> impl Responder {
     "POST Guild Inventory"
 }
