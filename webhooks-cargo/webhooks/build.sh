cc9c3a6cd2de8f776f4187d3f92f353660079659
Yoann Kehler <privat@yoann.de>
2019-04-10 09:00:03 +0200

    Deploy also static files

--- webhooks-cargo/webhooks/build.sh
+++ webhooks-cargo/webhooks/build.sh
@@ -8,13 +8,19 @@ rm -rf ./*
 git clone --recursive https://github.com/RPGLibrarium/Liberation.git
 cd Liberation
 
-dir="liberation.$(date -Iseconds)"
+date="$(date -Iseconds)"
+releasedir="liberation.release.$date"
+staticdir="liberation.static.$date"
 
-cargo build --release --target-dir  /target/"$dir"
+cargo build --release --target-dir  /target/"$releasedir"
+
+# copy static files
+cp -R /web/app /target/"$staticdir"
 
 # change symlink
 cd /target
-ln -sfn ./"$dir" ./liberation
+ln -sfn ./"$releasedir" ./liberation.release
+ln -sfn ./"$releasedir" ./liberation.static
 
 # cleanup -> 2 days or older may be deleted!
 find . -maxdepth 1 -mindepth 1 -type d -mtime +2 -exec rm -rf "{}" \;
